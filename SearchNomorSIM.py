f9e90ca3607817b1b4ed257ff564b8c675ef3bc6
ferdyirawanf <ferdyirawanf@gmail.com>
2020-08-10 19:40:49 +0700

    koreksi kalo error

--- SearchNomorSIM.py
+++ SearchNomorSIM.py
@@ -1,23 +1,12 @@
-# Menghitung jumlah baris pada array arrData
-baris = 0
-for i in arrData:
-    baris += 1
-
-col_nomor = [x[0] for x in arrData] # Membuat list yang berisi 1 kolom nomor SIM saja
-
-# Proses Pencarian Nomor SIM
-nomor = input("Masukkan Nomor SIM Anda: ")
-
-#Fungsi untuk search apakah nomor SIM sesuai
-def search():
-    for x in range(0,baris): # Looping untuk baris ke-0 sampai jumlah baris pada arrData
-        if nomor == col_nomor[x]: # Jika input nomor SIM samadengan data pada col_nomor baris ke-x
-            print(arrData[x]) # Mencetak pada layar semua data pada baris ke-x
-            return True
-    return False
-
-# Pengulangan jika nomor SIM tidak sesuai
-while search() == False:
-    print("Maaf nomor SIM Anda tidak terdaftar dalam sistem kami. Silakan coba lagi!")
-    nomor = input("Masukkan Nomor SIM Anda: ")
-    search()
+def search(dataSIM, nomorSIM):
+    found_nomor = False
+    while not found_nomor:
+        nomor = input("Masukkan Nomor SIM Anda: ")
+        for i in dataSIM:
+            if i[0] == nomor:
+                print(arrData[i])
+                found_nomor = True # Looping berhenti
+            else:
+                print("Maaf nomor SIM Anda tidak terdaftar dalam sistem kami. Silakan coba lagi!")
+            break
+    return data SIM
